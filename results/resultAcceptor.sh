276557e0378a9f66a92e28857d2d3b6f7e2cf10f
edwardhieatt <edwardhieatt@9c957974-bd0e-0410-960b-faf4f3e8a3c7>
2003-07-26 16:45:36 +0000

    *** empty log message ***

--- results/resultAcceptor.sh
+++ results/resultAcceptor.sh
@@ -1,2 +1,2 @@
-export CLASSPATH=$CLASSPATH:./lib/jsunit.jar:./lib/org.mortbay.jetty-jdk1.2.jar:./lib/jdom.jar:./lib/javax.servlet.jar
+export CLASSPATH=$CLASSPATH:./lib/jsunit.jar:./lib/org.mortbay.jetty-jdk1.2.jar:./lib/jdom.jar:./lib/javax.servlet.jar:./lib/xerces.jar
 java net.jsunit.ResultAcceptor
