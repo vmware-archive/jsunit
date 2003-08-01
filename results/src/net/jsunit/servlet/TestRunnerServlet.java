f65f2cf1bc4a43006b00b0874cb85ffad96a43c7
edwardhieatt <edwardhieatt@9c957974-bd0e-0410-960b-faf4f3e8a3c7>
2003-08-01 16:43:20 +0000

    *** empty log message ***

--- results/src/net/jsunit/servlet/TestRunnerServlet.java
+++ results/src/net/jsunit/servlet/TestRunnerServlet.java
@@ -1,19 +1,16 @@
 package net.jsunit.servlet;
-
 import java.io.IOException;
 import java.io.OutputStream;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import junit.framework.TestResult;
 import junit.textui.TestRunner;
 import net.jsunit.StandaloneTest;
-
 public class TestRunnerServlet extends HttpServlet {
 	protected synchronized void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Received request to run standalone test...");
 		StandaloneTest test = new StandaloneTest("testStandaloneRun");
 		test.setStartAndStopServer(false);
 		TestResult result = TestRunner.run(test);
@@ -22,5 +19,6 @@ public class TestRunnerServlet extends HttpServlet {
 		String resultString = result.wasSuccessful() ? "success" : "failure";
 		out.write(("<result>" + resultString + "</result>").getBytes());
 		out.close();
+		System.out.println("...done");
 	}
 }
