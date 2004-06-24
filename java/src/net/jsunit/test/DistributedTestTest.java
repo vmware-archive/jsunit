e83c7dd50b65aa9e74b5ba11b9349f2da81a708d
edwardhieatt <edwardhieatt@9c957974-bd0e-0410-960b-faf4f3e8a3c7>
2004-06-24 16:27:03 +0000

    no message

---
+++ java/src/net/jsunit/test/DistributedTestTest.java
@@ -0,0 +1,20 @@
+package net.jsunit.test;
+
+import net.jsunit.DistributedTest;
+import net.jsunit.JsUnitServer;
+
+public class DistributedTestTest extends DistributedTest {
+    public DistributedTestTest(String name) {
+        super(name);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        JsUnitServer.start();
+    }
+
+    public void tearDown() throws Exception {
+        JsUnitServer.stop();
+        super.tearDown();
+    }
+}
