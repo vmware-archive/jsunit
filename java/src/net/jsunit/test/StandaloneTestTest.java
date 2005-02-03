899055f46234fdd1a61043d9545c2529aeb72310
edwardhieatt <edwardhieatt@9c957974-bd0e-0410-960b-faf4f3e8a3c7>
2005-02-03 22:24:25 +0000

    no message

--- java/src/net/jsunit/test/StandaloneTestTest.java
+++ java/src/net/jsunit/test/StandaloneTestTest.java
@@ -6,20 +6,20 @@ import net.jsunit.StandaloneTest;
 public class StandaloneTestTest extends StandaloneTest {
 
   public StandaloneTestTest(String name) {
-    super(name);
+      super(name);
   }
 
   public void setUp() throws Exception {
-    System.setProperty(Configuration.BROWSER_FILE_NAMES, DEFAULT_SYSTEM_BROWSER + "," + DEFAULT_SYSTEM_BROWSER);
-    System.setProperty(Configuration.URL,
-       "http://localhost:8080/jsunit/testRunner.html?"
-       + "testPage=http://localhost:8080/jsunit/tests/jsUnitTestSuite.html&autoRun=true&submitresults=true&resultId=foobar");
-    super.setUp();
+      System.setProperty(Configuration.BROWSER_FILE_NAMES, DEFAULT_SYSTEM_BROWSER + "," + DEFAULT_SYSTEM_BROWSER);
+      System.setProperty(Configuration.URL,
+         "http://localhost:8080/jsunit/testRunner.html?"
+         + "testPage=http://localhost:8080/jsunit/tests/jsUnitTestSuite.html&autoRun=true&submitresults=true&resultId=foobar");
+      super.setUp();
   }
 
   public void tearDown() throws Exception {
-    System.getProperties().remove(Configuration.BROWSER_FILE_NAMES);
-    System.getProperties().remove(Configuration.URL);
-    super.tearDown();
+      System.getProperties().remove(Configuration.BROWSER_FILE_NAMES);
+      System.getProperties().remove(Configuration.URL);
+      super.tearDown();
   }
 }
