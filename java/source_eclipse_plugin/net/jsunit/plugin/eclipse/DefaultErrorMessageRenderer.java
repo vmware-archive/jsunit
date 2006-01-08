ab05c384a7f942e2325f30fc1c9bf98a2e2d7a6c
edwardhieatt <edwardhieatt@9c957974-bd0e-0410-960b-faf4f3e8a3c7>
2006-01-08 21:54:19 +0000

    *** empty log message ***

--- java/source_eclipse_plugin/net/jsunit/plugin/eclipse/DefaultErrorMessageRenderer.java
+++ java/source_eclipse_plugin/net/jsunit/plugin/eclipse/DefaultErrorMessageRenderer.java
@@ -5,8 +5,12 @@ import org.eclipse.swt.widgets.Shell;
 
 public class DefaultErrorMessageRenderer implements ErrorMessageRenderer {
 
-	public void showError(String title, String message) {
-		MessageDialog.openInformation(new Shell(), title, message);
+	public void showError(final String title, final String message) {
+		JsUnitPlugin.getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				MessageDialog.openInformation(new Shell(), title, message);
+			}
+		});
 	}
 
 }
