a5bf07ee60f685d10b6ec3c6194149aad30b206c
edwardhieatt <edwardhieatt@9c957974-bd0e-0410-960b-faf4f3e8a3c7>
2006-02-19 02:56:06 +0000

    *** empty log message ***

--- java/source_eclipse_plugin/net/jsunit/plugin/eclipse/preference/PreferenceInitializer.java
+++ java/source_eclipse_plugin/net/jsunit/plugin/eclipse/preference/PreferenceInitializer.java
@@ -13,7 +13,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		store.setDefault(PreferenceConstants.PREFERENCE_BROWSER_FILE_NAMES, "");
 		store.setDefault(PreferenceConstants.PREFERENCE_CLOSE_BROWSERS_AFTER_TEST_RUNS, Boolean.valueOf(ConfigurationProperty.CLOSE_BROWSERS_AFTER_TEST_RUNS.getDefaultValue()));
 		store.setDefault(PreferenceConstants.PREFERENCE_INSTALLATION_DIRECTORY, "");
-		store.setDefault(PreferenceConstants.PREFERENCE_LOG_STATUS, Boolean.valueOf(ConfigurationProperty.LOG_STATUS.getDefaultValue()));
+		store.setDefault(PreferenceConstants.PREFERENCE_LOG_STATUS, false);
 		store.setDefault(PreferenceConstants.PREFERENCE_TEST_PAGE_EXTENSIONS, "html,htm");
 		store.setDefault(PreferenceConstants.PREFERENCE_TIMEOUT_SECONDS, ConfigurationProperty.TIMEOUT_SECONDS.getDefaultValue());
 		store.setDefault(PreferenceConstants.PREFERENCE_USE_DEFAULT_LOGS_DIRECTORY, true);
