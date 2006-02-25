ea9dbd6d3a13134e3bc4a1eb0adcc86997c4d9eb
edwardhieatt <edwardhieatt@9c957974-bd0e-0410-960b-faf4f3e8a3c7>
2006-02-25 20:07:08 +0000

    no message

--- java/source_server/net/jsunit/DistributedTest.java
+++ java/source_server/net/jsunit/DistributedTest.java
@@ -1,32 +1,76 @@
 package net.jsunit;
 
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 import net.jsunit.configuration.Configuration;
 import net.jsunit.configuration.ConfigurationSource;
+import net.jsunit.configuration.DelegatingConfigurationSource;
 import net.jsunit.logging.NoOpStatusLogger;
 import net.jsunit.model.FarmTestRunResult;
 import net.jsunit.utility.XmlUtility;
+import org.mortbay.util.MultiException;
+
+import java.net.URL;
+import java.net.BindException;
+import java.util.List;
 
 public class DistributedTest extends TestCase {
 
     protected DistributedTestRunManager manager;
     private JsUnitServer server;
+    private ConfigurationSource farmConfigurationSource;
 
     public DistributedTest(String name) {
         super(name);
+        this.farmConfigurationSource = farmConfigurationSource();
+    }
+
+    public DistributedTest(ConfigurationSource source) {
+        this(source.remoteMachineURLs());
+        this.farmConfigurationSource = source;
     }
 
     public void setUp() throws Exception {
         super.setUp();
         server = new JsUnitServer(new Configuration(configurationSource()));
-        server.start();
+        startServerIfNecssary();
         manager = createTestRunManager();
     }
 
+    private void startServerIfNecssary() throws Exception {
+        try {
+            server.start();
+        } catch (MultiException e) {
+            List exceptions = e.getExceptions();
+            if (exceptions.size() == 1 && exceptions.get(0) instanceof BindException) {
+                //if a server is already running, fine - we only need it to serve content to remote machines
+            } else
+                throw e;
+        }
+    }
+
     public void tearDown() throws Exception {
-        if (server != null)
+        if (server != null && server.isAlive())
             server.dispose();
         super.tearDown();
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        ConfigurationSource originalSource = Configuration.resolveSource();
+        Configuration configuration = new Configuration(originalSource);
+        for (final URL remoteMachineURL : configuration.getRemoteMachineURLs())
+            suite.addTest(new DistributedTest(new DelegatingConfigurationSource(originalSource) {
+                public String remoteMachineURLs() {
+                    return remoteMachineURL.toString();
+                }
+            }));
+        return suite;
+    }
+
+    protected void runTest() throws Throwable {
+        testCollectResults();
     }
 
     public void testCollectResults() {
@@ -46,7 +90,7 @@ public class DistributedTest extends TestCase {
     }
 
     protected DistributedTestRunManager createTestRunManager() {
-        return new DistributedTestRunManager(new NoOpStatusLogger(), new Configuration(farmConfigurationSource()));
+        return new DistributedTestRunManager(new NoOpStatusLogger(), new Configuration(farmConfigurationSource));
     }
 
     protected ConfigurationSource farmConfigurationSource() {
