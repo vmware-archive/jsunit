6ca39d5e5771cb0f295321514ded5fe82ebc9c3f
edwardhieatt <edwardhieatt@9c957974-bd0e-0410-960b-faf4f3e8a3c7>
2006-03-20 17:57:18 +0000

    Adding the ability to specify a particular browserId when requesting a run.

---
+++ java/tests_server/net/jsunit/ServerLandingPageFunctionalTest.java
@@ -0,0 +1,48 @@
+package net.jsunit;
+
+import net.jsunit.model.BrowserResult;
+import net.jsunit.utility.SystemUtility;
+import net.jsunit.utility.XmlUtility;
+import org.jdom.Document;
+
+public class ServerLandingPageFunctionalTest extends FunctionalTestCase {
+
+    public void testSlash() throws Exception {
+        webTester.beginAt("/");
+        assertOnLandingPage();
+        webTester.assertTextPresent(SystemUtility.osString());
+        webTester.assertTextPresent(SystemUtility.hostname());
+        webTester.assertTextPresent(SystemUtility.ipAddress());
+        webTester.assertLinkPresentWithText(new FunctionalTestConfigurationSource(port).url());
+    }
+
+    public void testIndexDotJsp() throws Exception {
+        webTester.beginAt("/index.jsp");
+        assertOnLandingPage();
+    }
+
+    public void testConfigLink() throws Exception {
+        webTester.beginAt("/");
+        webTester.clickLinkWithText("config");
+        assertConfigXml();
+    }
+
+    public void testDisplayerForm() throws Exception {
+        server.launchBrowserTestRun(new BrowserLaunchSpecification(BrowserLaunchSpecification.DEFAULT_SYSTEM_BROWSER));
+        BrowserResult browserResult = new BrowserResult();
+        String id = String.valueOf(System.currentTimeMillis());
+        browserResult.setId(id);
+        server.accept(browserResult);
+        webTester.beginAt("/");
+        webTester.setWorkingForm("displayerForm");
+        webTester.setFormElement("id", id);
+        webTester.setFormElement("browserId", "0");
+        webTester.submit();
+        assertEquals(XmlUtility.asString(new Document(browserResult.asXml())), webTester.getDialog().getResponseText());
+    }
+
+    private void assertOnLandingPage() {
+        webTester.assertTitleEquals("JsUnit  Server");
+    }
+
+}
