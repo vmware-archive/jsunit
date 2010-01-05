957a0097bd4bf1affd6f25acfe6a7da3b5206278
David Goudreau & Sam Pierson <pair+david+sam@pivotallabs.com>
2010-01-04 17:58:38 -0800

    Added bulid target selenium_sauce which runs tests at saucelabs.com
    Selenium config is set via XML file ../selenium.xml with format:
    
    <selenium_config name="saucelabs">
        <host>saucelabs.com</host>
        <port>4444</port>
        <browser_start_command>YOUR CREDENTIALS</browser_start_command>
        <browser_url>ADDRESS OF TUNNEL TO REACH YOUR WORKSTATION FROM SAUCELABS</browser_url>
    </selenium_config>

--- java/source_server/net/jsunit/SeleniumTest.java
+++ java/source_server/net/jsunit/SeleniumTest.java
@@ -5,10 +5,17 @@ import com.thoughtworks.selenium.Selenium;
 import com.thoughtworks.selenium.Wait;
 import junit.framework.TestCase;
 import net.jsunit.configuration.CompositeConfigurationSource;
-import net.jsunit.configuration.ConfigurationSource;
 import net.jsunit.configuration.DelegatingConfigurationSource;
 import net.jsunit.configuration.ServerConfiguration;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,33 +31,27 @@ public class SeleniumTest extends TestCase {
 
     public void setUp() throws Exception {
         super.setUp();
-        server = new JsUnitServer(new ServerConfiguration(new DelegatingConfigurationSource(CompositeConfigurationSource.resolve())));
-        server.start();
-
-        selenium = new DefaultSelenium(System.getProperty(SELENIUM_SERVER_HOST),
-                Integer.parseInt(System.getProperty(SELENIUM_SERVER_PORT)),
-                System.getProperty(SELENIUM_BROWSER_STARTCOMMAND),
-                System.getProperty(SELENIUM_BROWSER_URL));
-        selenium.start();
+        startJsUnitServer();
+        startSeleniumClient();
     }
 
     public void tearDown() throws Exception {
-        if (server != null) {
-            server.dispose();
-        }
-
-        selenium.stop();
+        stopJsUnitServer();
+        stopSeleniumClient();
         super.tearDown();
     }
 
     public void testStandaloneRun() throws Exception {
-        selenium.open("http://localhost:8080/jsunit/testRunner.html?testPage=/jsunit/tests/jsUnitTestSuite.html&autorun=true");
+        selenium.open("/jsunit/testRunner.html?testPage=/jsunit/tests/jsUnitTestSuite.html&autorun=true");
         waitForStatus();
 
         // window.frames['mainFrame'].frames['mainCounts'].frames['mainCountsRuns'].document.getElementById('content').textContent
-        String runs = selenium.getEval("window.mainFrame.mainCounts.mainCountsRuns.document.getElementById('content').textContent").trim();
-        String fails = selenium.getEval("window.mainFrame.mainCounts.mainCountsFailures.document.getElementById('content').textContent").trim();
-        String errors = selenium.getEval("window.mainFrame.mainCounts.mainCountsErrors.document.getElementById('content').textContent").trim();
+        String runs = selenium.getEval("window.frames['mainFrame'].frames['mainCounts'].frames['mainCountsRuns'].document.getElementById('content').textContent").trim();
+        String fails = selenium.getEval("window.frames['mainFrame'].frames['mainCounts'].frames['mainCountsFailures'].document.getElementById('content').textContent").trim();
+        String errors = selenium.getEval("window.frames['mainFrame'].frames['mainCounts'].frames['mainCountsErrors'].document.getElementById('content').textContent").trim();
+//        String runs = selenium.getEval("window.mainFrame.mainCounts.mainCountsRuns.document.getElementById('content').textContent").trim();
+//        String fails = selenium.getEval("window.mainFrame.mainCounts.mainCountsFailures.document.getElementById('content').textContent").trim();
+//        String errors = selenium.getEval("window.mainFrame.mainCounts.mainCountsErrors.document.getElementById('content').textContent").trim();
 
         Pattern pattern = Pattern.compile("\\d+$");
         Matcher matcher = pattern.matcher(runs);
@@ -69,14 +70,71 @@ public class SeleniumTest extends TestCase {
                 runCount, failCount, errorCount);
 
         if (failCount + errorCount > 0) {
-            String errorMessages = selenium.getEval("window.mainFrame.mainErrors.document.getElementsByName('problemsList')[0].innerHTML");
+            String errorMessages = selenium.getEval("window.frames['mainFrame'].frames['mainErrors'].document.getElementsByName('problemsList')[0].innerHTML");
             System.out.printf("Error messages: %s\n", errorMessages);
 
         }
         assertEquals(0, failCount + errorCount);
     }
 
-    public void waitForStatus() throws InterruptedException {
+    private HashMap<String, String> loadSeleniumConfig(String configPath) throws Exception {
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse (new File(configPath));
+        doc.getDocumentElement().normalize();
+        Element seleniumConfig = (Element)doc.getElementsByTagName("selenium_config").item(0);
+        HashMap<String, String> seleniumProperties = new HashMap<String, String>();
+
+        String host = System.getProperty(SELENIUM_SERVER_HOST);
+        String port = System.getProperty(SELENIUM_SERVER_PORT);
+        String start_command = System.getProperty(SELENIUM_BROWSER_STARTCOMMAND);
+        String browser_url = System.getProperty(SELENIUM_BROWSER_URL);
+
+        if (host == null) {
+          host = seleniumConfig.getElementsByTagName("host").item(0).getFirstChild().getNodeValue();
+        }
+        if (port == null) {
+          port = seleniumConfig.getElementsByTagName("port").item(0).getFirstChild().getNodeValue();
+        }
+        if (start_command == null) {
+          start_command = seleniumConfig.getElementsByTagName("browser_start_command").item(0).getFirstChild().getNodeValue();
+        }
+        if (browser_url == null) {
+          browser_url = seleniumConfig.getElementsByTagName("browser_url").item(0).getFirstChild().getNodeValue();
+        }
+        seleniumProperties.put("host", host);
+        seleniumProperties.put("port", port);
+        seleniumProperties.put("browser_start_command", start_command);
+        seleniumProperties.put("browser_url", browser_url);
+        return seleniumProperties;
+    }
+
+    private void startJsUnitServer() throws Exception {
+        server = new JsUnitServer(new ServerConfiguration(new DelegatingConfigurationSource(CompositeConfigurationSource.resolve())));
+        server.start();
+    }
+
+    private void stopJsUnitServer() {
+        if (server != null) {
+            server.dispose();
+        }
+    }
+
+    private void startSeleniumClient() throws Exception {
+        HashMap<String, String> seleniumConfig = loadSeleniumConfig("../selenium.xml");
+        String host = seleniumConfig.get("host");
+        int port = Integer.parseInt(seleniumConfig.get("port"));
+        String start_command = seleniumConfig.get("browser_start_command");
+        String browser_url = seleniumConfig.get("browser_url");
+        selenium = new DefaultSelenium(host, port, start_command, browser_url);
+        selenium.start();
+    }
+
+    private void stopSeleniumClient() {
+        selenium.stop();
+    }
+
+    private void waitForStatus() throws InterruptedException {
         new Wait() {
             public boolean until() {
                 String statusContent = selenium.getEval("window.mainFrame.mainStatus.document.getElementById('content').textContent");
